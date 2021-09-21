23b052b7d6cd2ca327de72e0f82a5e9cafa8e561
butvin <butvin.sergey@gmail.com>
2021-09-21 13:21:45 +0300

    REALIZE Galera Box Symfony-5.3.7 PHP 8.0.10 Nginx 1.21.3

---
+++ migrations/Version20210921094309.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210921094309 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE app (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) DEFAULT NULL, external_id VARCHAR(127) NOT NULL, UNIQUE INDEX UNIQ_C96E70CF9F75D7B0 (external_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime)\', INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE app');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
