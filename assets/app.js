fc6a481bff5860498250f83ba6b0e57952056431
butvin <butvin.sergey@gmail.com>
2021-10-08 03:32:14 +0300

    docker-compose 3.9. stable-v0.9.3

--- assets/app.js
+++ assets/app.js
@@ -1,38 +1,14 @@
+/** FontAwesome 5 */
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/js/all.min.js';
+
 /** Main javascript entrypoint content here is. */
-const entrypoint = {
-    js: {
-        fileName: ['app.js'],
-        path: './',
-    },
-    css: {
-        fileName: 'app.css',
-        path: 'styles/',
-    }
-}
-console.log(`hello >>> 'assets/' ${entrypoint.js.fileName}`)
-
-
-
-
-
-
-
-
-
 import './styles/app.css'
+console.log(`ping entrypoint`)
 
 // import '@modern-normalize/modern-normalize.css';
-// import '@modern-normalize/modern-normalize.css';
-
-//** FontAwesome 5 */
-// import '@fortawesome/fontawesome-free/css/all.css';
-// import '@fortawesome/fontawesome-free/js/all.min.js';
 
 // import 'tailwindcss/tailwind.css'
-
-/** Import CSS & SCSS files */
-
-// import './styles/main.css'
 
 
 
--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -1,20 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class AppController extends AbstractController
 {
-    public function index(): Response
+    final public function index(Request $request): Response
     {
-        $data = $_SERVER;
-
         return $this->render('app/index.html.twig', [
-            'controller_name' => 'AppController',
-            'data' => $data,
+            'data' => $request,
         ]);
     }
 }
